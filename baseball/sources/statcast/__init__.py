6737b0e626266ed9ee238c348f8af744b137c030
Blaine Winslow <blaine.winslow@gmail.com>
2026-05-11 12:03:16 -0400

    feat(statcast): export StatcastDownloader and StatcastIngestor from sources/statcast __init__

--- baseball/sources/statcast/__init__.py
+++ baseball/sources/statcast/__init__.py
@@ -3,3 +3,11 @@
 Handles downloading and ingesting StatCast data from Baseball Savant.
 Uses pybaseball as internal implementation but provides baseball namespace API.
 """
+
+from baseball.sources.statcast.downloader import StatcastDownloader
+from baseball.sources.statcast.ingestor import StatcastIngestor
+
+__all__ = [
+    "StatcastDownloader",
+    "StatcastIngestor",
+]

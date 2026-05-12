080660510954ad0edac7caa152b9500720013c1b
Blaine Winslow <blaine.winslow@gmail.com>
2026-05-12 18:12:25 -0400

    feat(lahman): export LahmanDownloader from subpackage __init__

--- baseball/sources/lahman/__init__.py
+++ baseball/sources/lahman/__init__.py
@@ -3,3 +3,7 @@
 Handles downloading and ingesting historical baseball data from
 Sean Lahman's comprehensive baseball statistics database.
 """
+
+from baseball.sources.lahman.downloader import LahmanDownloader
+
+__all__ = ["LahmanDownloader"]
